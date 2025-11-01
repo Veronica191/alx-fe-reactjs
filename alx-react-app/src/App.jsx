e35a836bda93fc1ee1b6386725fc125f8303d33f
Veronica Akwojinga <veronijinga2003@gmail.com>
2025-11-01 10:27:05 +0000

    Added UserProfile components and modified App.jsx

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -6,6 +6,8 @@ import WelcomeMessage from './components/WelcomeMessage'
 import Header from './components/Header'
 import MainContent from './components/MainContent'
 import Footer from './components/Footer'
+import UserProfile from './components/UserProfile'
+
 
 function App() {
   const [count, setCount] = useState(0)
@@ -19,6 +21,11 @@ function App() {
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
+        <UserProfile 
+        name="Alice" 
+        age="25" 
+        bio="Loves hiking and photography" 
+      />
       </div>
 
       <h1>Vite + React</h1>
@@ -28,6 +35,7 @@ function App() {
       <Header />
       <MainContent />
       <Footer />
+      <UserProfile />
     </>
   )
 }
