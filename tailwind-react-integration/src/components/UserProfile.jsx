2944ba7bb94e65aaab4abe58b3824fb103e851ac
Veronica Akwojinga <veronijinga2003@gmail.com>
2025-11-26 08:18:04 +0000

    Responsive Design using tailwind css

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -4,12 +4,12 @@ function UserProfile() {
       <img 
         src="https://via.placeholder.com/150" 
         alt="User" 
-        className="rounded-full w-36 h-36 mx-auto"
+        className="rounded-full w-24  h-24 md:w-36 md:h-36 mx-auto"
       />
-      <h1 className="text-xl text-blue-800 my-4">
+      <h1 className="text-lg md:text-xl text--blue-800 my-4">   
         John Doe
       </h1>
-      <p className="text-gray-600 text-base">
+      <p className="text-gray-600 text-sm md:text-base">    
         Developer at Example Co. Loves to write code and explore new technologies.
       </p>
     </div>
